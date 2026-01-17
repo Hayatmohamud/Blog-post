1098a147833a9a27b0a24f4898111207a36ac90a
Hayatmohamud <hayadmohamudhassan@gmail.com>
2026-01-17 12:48:00 +0300

    added nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
+import { Routes , Route} from "react-router"
+import Home from "./pages/Home"
+import Article from "./pages/Article";
+import Articles from "./pages/Articles";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 
 
 function App() {
   
 
   return (
-    <>
- 
-    </>
-  )
+    <div>
+      {/* Header */}
+
+      <main>
+        {/* public routes */}
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/article" element={<Article />} />
+          <Route path="article/:id" element={<Articles />} />
+
+          {/* unauthenticated routes (rediracte to home if logged in) */}
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </main>
+      {/* footer */}
+    </div>
+  );
 }
 
 export default App
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <div>
+      <H1>SigUp page</H1>
+    </div>
+  );
+}
+
+export default SignUp;
